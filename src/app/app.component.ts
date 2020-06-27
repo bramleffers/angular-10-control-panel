d9d393b48b32a0fb12ecbba5b4fb99f718d5a37e
bramleffers <bramleffers@gmail.com>
2020-06-28 00:15:22 +0200

    Added Material 10, 5 files modified, 2 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, VERSION } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatLabel, MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+@NgModule({ 
+  imports:      [ BrowserModule, FormsModule, MatTableModule, MatExpansionModule, MatIconModule, MatFormFieldModule, MatDatepickerModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
